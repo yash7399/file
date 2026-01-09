a9625e31f37905a2a64359053d56cdc6dee3d684
Yash Shukla <119918899+yash7399@users.noreply.github.com>
2026-01-09 09:34:00 +0530

    Create FileConfig.java

---
+++ FileConfig.java
@@ -0,0 +1,36 @@
+package com.example.sft.config;
+
+public class FileConfig {
+	
+	private String filename;
+	private String source;
+	private String destination;
+	private int time;
+	
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	
+	
+}
