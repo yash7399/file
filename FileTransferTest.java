b712cc1d2c4f7f3f5718f5ee3f028ab2b12fb142
Yash Shukla <119918899+yash7399@users.noreply.github.com>
2026-01-09 09:30:22 +0530

    Create FileTransferTest.java

---
+++ FileTransferTest.java
@@ -0,0 +1,14 @@
+package com.example.sft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+public class FileTransferTest {
+    public static void main(String[] args) {
+    
+    		SpringApplication.run(FileTransferTest.class, args);
+    		
+    }
+}
